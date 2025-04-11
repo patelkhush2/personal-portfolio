17a4e612815a397c0aaef421a3b8608715ac280a
Khush Patel <khussshpatel@gmail.com>
2025-04-10 20:31:50 -0400

    Add files via upload

--- index.vue
+++ index.vue
@@ -8,23 +8,6 @@
   
     <h1>Undergoing Construction.</h1>
     <p>Khush Patel © 2025</p>
-   <!-- <h1>Hi, I'm Khush 👋</h1>
-    <div class="container">
-      <div class="grid">
-        <NuxtLink to="/work" class="item">
-          <span>Work</span>
-        </NuxtLink>
-        <NuxtLink to="/about" class="item">
-          <span>About Me</span>
-        </NuxtLink>
-        <NuxtLink to="/contact" class="item">
-          <span>Contact</span>
-        </NuxtLink>
-        <NuxtLink to="/blog" class="item">
-          <span>Blog</span> 
-        </NuxtLink>
-      </div>
-    </div>-->
   
   </div>
 </template>
